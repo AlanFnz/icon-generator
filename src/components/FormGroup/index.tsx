cf76d71297d616adc4fcd50d21224e28cb963cb8
Alan Fernandez <alan.f@msn.com>
2023-06-13 22:30:36 +0100

    generate page and form components

---
+++ src/components/FormGroup/index.tsx
@@ -0,0 +1,5 @@
+const FormGroup = (props: React.ComponentPropsWithoutRef<"div">) => {
+  return <div {...props} className="flex flex-col gap-1">{props.children}</div>;
+};
+
+export default FormGroup;
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,5 @@
+const Input = (props: React.ComponentPropsWithoutRef<"input">) => {
+  return <input {...props} type="text" className="border border-gray-800"></input>;
+};
+
+export default Input;
