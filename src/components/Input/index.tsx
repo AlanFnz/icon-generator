730cad7018d4543950e6335e63b5389dad52eb23
Alan Fernandez <alan.f@msn.com>
2023-06-16 20:39:25 +0100

    style tweaks on input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,11 @@
 const Input = (props: React.ComponentPropsWithoutRef<"input">) => {
-  return <input {...props} type="text" className="border border-gray-800"></input>;
+  return (
+    <input
+      {...props}
+      type="text"
+      className="py-2 rounded border border-gray-800 px-2 dark:text-gray-800"
+    ></input>
+  );
 };
 
 export default Input;
